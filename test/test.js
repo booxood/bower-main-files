9d3d95532a4ce2914b41e0b4211e6aeb18694ca9
Liucw <liucw.mail@gmail.com>
2014-11-18 17:40:51 +0800

    Init project

---
+++ test/test.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var should = require('should');
+
+describe('bower-main-files test case', function() {
+
+    describe('ok', function() {
+        before(function() {});
+
+        it('ok', function() {
+        });
+    });
+
+});
+
